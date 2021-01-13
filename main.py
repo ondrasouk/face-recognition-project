17f2ec359e66d35e7302dc876a1ce72bc190233b
ondrasouk <ondrasouk@seznam.cz>
2021-01-13 12:30:38 +0100

    Moved mouse_move to another thread Class PerpetualTimer is used to repeatedly execute function mouse_move every 0.01 s. mouse_move_wrap is used to pass variables

--- main.py
+++ main.py
@@ -5,55 +5,102 @@ from face_detect import face_detect
 from draw_tool import draw_rect, draw_point, draw_lines
 from mouse_move import mouse_move
 import pyautogui
-
-cam = cv2.VideoCapture(0)
-cam_dim = [cam.get(cv2.CAP_PROP_FRAME_WIDTH), cam.get(cv2.CAP_PROP_FRAME_HEIGHT)]
-
-
-# init reference point coordinates
-reference_point = []
-
-# turn off failsafe (when True cursor hits top-left corner of the screen and program fails)
-pyautogui.FAILSAFE = False
+import threading
+import time
 
 
-while True:
-    # Capturing images
-    ret, frame = cam.read()
-    # Mirror webcam
-    frame = cv2.flip(frame, 1)
-    # Detecting
-    center_face_position, face_position = face_detect(frame)  # detect face position
-    if len(face_position) != 0:
-        if len(reference_point):
-            mouse_move(reference_point, center_face_position, cam_dim)
-            pass
-        else:  # Use first face frame as reference
-            reference_point = center_face_position
-            print("Reference set as:", reference_point)
-        # Marking in frame
-        edited_frame = draw_point(frame, center_face_position, 0)  # mark face center point
-        edited_frame = draw_point(edited_frame, reference_point, 1)  # mark face center point
-        edited_frame = draw_lines(edited_frame, reference_point, center_face_position)  # draw all lines
-        edited_frame = draw_rect(edited_frame, face_position)  # mark face
+class PerpetualTimer:
 
-        # Display the resulting frame
-        cv2.imshow('frame', edited_frame)
-    else:
-        cv2.imshow('frame', frame)
+    def __init__(self, seconds, target):
+        self._should_continue = False
+        self.is_running = False
+        self.seconds = seconds
+        self.target = target
+        self.thread = None
 
-    # Keystroke detect:
-    k = cv2.waitKey(1)
-    if k > 0:  # keystroke detected
-        # quit on "Q"
-        if k == ord('q'):
-            break
-        # new reference point on "R"
-        if k == ord('r'):
-            if len(center_face_position) != 0:
+    def _handle_target(self):
+        self.is_running = True
+        self.target()
+        self.is_running = False
+        self._start_timer()
+
+    def _start_timer(self):
+        if self._should_continue:
+            self.thread = threading.Timer(self.seconds, self._handle_target)
+            self.thread.start()
+
+    def start(self):
+        if not self._should_continue and not self.is_running:
+            self._should_continue = True
+            self._start_timer()
+
+    def cancel(self):
+        if self.thread is not None:
+            self._should_continue = False
+            self.thread.cancel()
+
+
+if __name__ == '__main__':
+    
+    def mouse_move_wrap():
+        mouse_move(reference_point, center_face_position, cam_dim)
+
+    cam = cv2.VideoCapture(0)
+    global cam_dim
+    cam_dim = [cam.get(cv2.CAP_PROP_FRAME_WIDTH), cam.get(cv2.CAP_PROP_FRAME_HEIGHT)]
+
+    # init reference point coordinates
+    global reference_point
+    global center_face_position
+    center_face_position = []
+    reference_point = []
+
+    # turn off failsafe (when True cursor hits top-left corner of the screen and program fails)
+    pyautogui.FAILSAFE = False
+
+    t = PerpetualTimer(0.01, mouse_move_wrap)
+    t.start()
+
+    while True:
+        # Capturing images
+        ret, frame = cam.read()
+        # Mirror webcam
+        frame = cv2.flip(frame, 1)
+        # Detecting
+        center_face_position, face_position = face_detect(frame)  # detect face position
+        if len(face_position) != 0:
+            if len(reference_point):
+                # mouse_move(reference_point, center_face_position, cam_dim)
+                pass
+            else:  # Use first face frame as reference
                 reference_point = center_face_position
                 print("Reference set as:", reference_point)
+            # Marking in frame
+            edited_frame = draw_point(frame, center_face_position, 0)  # mark face center point
+            edited_frame = draw_point(edited_frame, reference_point, 1)  # mark face center point
+            edited_frame = draw_lines(edited_frame, reference_point, center_face_position)  # draw all lines
+            edited_frame = draw_rect(edited_frame, face_position)  # mark face
 
-# Release handle to the webcam
-cam.release()
-cv2.destroyAllWindows()
+            # Display the resulting frame
+            cv2.imshow('frame', edited_frame)
+        else:
+            cv2.imshow('frame', frame)
+
+        # Keystroke detect:
+        k = cv2.waitKey(1)
+        if k > 0:  # keystroke detected
+            # quit on "Q"
+            if k == ord('q'):
+                break
+            # new reference point on "R"
+            if k == ord('r'):
+                if len(center_face_position) != 0:
+                    reference_point = center_face_position
+                    print("Reference set as:", reference_point)
+
+    # Release handle to the webcam
+    t.cancel()
+    cam.release()
+    cv2.destroyAllWindows()
+
+
--- mouse_move.py
+++ mouse_move.py
@@ -15,16 +15,18 @@ SCALE_Y = 0.5
 
 
 def mouse_move(reference_point, center_face_position, cam_dim):
-    dev = [0, 0]
-    disp_dim = gui.size()
-    dev[0] = center_face_position[0] - reference_point[0]
-    dev[1] = center_face_position[1] - reference_point[1]
-    if (dev[0]**2+dev[1]**2) >= (RANGE_OF_INSENSITIVITY**2):
-        movX, movY = insensitivity(dev[0], dev[1])
-        gui.moveRel(movX, movY, 0)
-    return 0
+    if len(reference_point):
+        dev = [0, 0]
+        disp_dim = gui.size()
+        dev[0] = center_face_position[0] - reference_point[0]
+        dev[1] = center_face_position[1] - reference_point[1]
+        if (dev[0]**2+dev[1]**2) >= (RANGE_OF_INSENSITIVITY**2):
+            movX, movY = insensitivity(dev[0], dev[1])
+            gui.moveRel(movX, movY, 0)
+        return 0
 
 
 def insensitivity(x, y):
-    
+    x = 0.2*x
+    y = 0.2*y
     return x, y
